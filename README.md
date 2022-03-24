44a45c45083aca37ae633989559d2f8d47b6c1fe
timentraining <timentraining@gmail.com>
2022-03-24 16:32:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tzwaqv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tzwaqv)
---
+++ src/app/button-overview-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic buttons
+ */
+@Component({
+  selector: 'button-overview-example',
+  templateUrl: 'button-overview-example.html',
+  styleUrls: ['button-overview-example.css'],
+})
+export class ButtonOverviewExample {}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
